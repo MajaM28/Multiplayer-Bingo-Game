7056f759493e0e94a668bf7a6efc6be3e038dea7
Maja Markiewicz <maja.markiewicz28@gmail.com>
2026-02-02 16:02:03 +0100

    stylizuje i dodaje log out

--- frontend/src/components/gameCard.jsx
+++ frontend/src/components/gameCard.jsx
@@ -1,13 +1,16 @@
 export default function GameCard({ game }) {
   return (
     <div className="gamecardContainer">
-      <h3>{game.name}</h3>
-      <p>
-        Players: {game.players.length}/{game.maxPlayers}
-      </p>
-      <p>Status: {game.status}</p>
-      <button>Join Game</button>
-      <button>Watch Game</button>
+      <div className="gameInfo">
+        <h3>{game.name}</h3>
+        <p>
+          Players: {game.players.length}/{game.maxPlayers}
+        </p>
+        <p>Status: {game.status}</p>
+      </div>
+      <div className="buttonRight">
+        <button className="gameCardButton">Join Game</button>
+      </div>
     </div>
   );
 }
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("user");
+    navigate("/login");
+  }
   return (
     <div className="navbarContainer">
       <p>WELCOME TO BINGO ❀˖°</p>
-      <button className="logoutButton">Log out</button>
+      <button className="logoutButton" onClick={() => handleLogout()}>
+        Log out
+      </button>
     </div>
   );
 }
